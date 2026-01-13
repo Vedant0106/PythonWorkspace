29180404ca8853ebb8b58eca81706a6abbaf93db
Vedant0106 <srivastava.vedant0106@gmail.com>
2026-01-13 15:48:06 +0530

    first

--- functions/first.py
+++ functions/first.py
@@ -1,2 +1,28 @@
-def hello():
-    print(hello)
+# def greet_person(name: str, gender: str) -> None:
+#     # gender = gender.strip().lower()
+
+#     if gender == "male":
+#         title = "Mr"
+#     elif gender == "female":
+#         title = "Miss"
+#     else:
+#         title = ""
+
+#     if title:
+#         print(f"Hello {title} {name}")
+#     else:
+#         print(f"Hello {name}")
+
+
+# name_input = input("Enter your name: ")
+# gender_input = input("Enter your gender (male/female): ")
+
+# greet_person(name_input, gender_input)
+
+message = (
+    "Please join this group and give a run 🙏\n"
+    "Guys please copy this 💁🏻\n"
+    "https://chat.whatsapp.com/"
+)
+
+print(message)
